175c13dc38e5f77207e9e5cbbd273d682bfa97ee
ElWilly9 <wvelasquez@unal.edu.co>
2025-06-07 00:27:47 -0500

    voz a texto

---
+++ voz_text.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+import pyttsx3 #Text-to-Speech
+import os
+import time
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("escuchando...")
+    audio = r.listen(source)
+    # Guardar el audio en un archivo WAV
+    with open("query.wav", "wb") as f:
+        f.write(audio.get_wav_data())
+    try:    
+        print("Espere un momento, procesando...")
+        text = r.recognize_google(audio, language='es-ES')
+        time.sleep(5)
+        print("Texto reconocido:", text)
+        engine = pyttsx3.init()
+        engine.setProperty('rate', 120)  # Velocidad de habla
+        engine.setProperty('volume', 1)  # Volumen (0.0 a 1.0)
+        engine.say(text)
+        engine.runAndWait()
+    except:
+        print("Lo siento, no pude entender el audio.")
