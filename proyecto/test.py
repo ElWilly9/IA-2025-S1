1a90e2b8350bc56c53168344b632d4e2f538b4ea
ElWilly9 <wvelasquez@unal.edu.co>
2025-06-12 00:00:27 -0500

    metiendo groq

--- proyecto/test.py
+++ proyecto/test.py
@@ -1,13 +1,33 @@
 from google import genai
+import os
+from groq import Groq
 
-key = "AIzaSyDHgW3Dqou631Yb2BQV1eHPzv2OCXUfIR0"
-query = "conces acerca de la moto bajaj boxer ct100 KS?"
+query = (
+    "¿Sabes acerca de la bocer ct100 ks? Responde en español, con un tono amigable, como si fueras un amigo contando algo interesante. "
+    "No digas que eres una IA ni expliques cómo sabes la información. Solo responde directamente."
+)
 
-client = genai.Client(api_key=key)
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents=query,
 )
 print(response.text)
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": query,
+        }
+    ],
+    model="deepseek-r1-distill-llama-70b",
+)
+
+print(chat_completion.choices[0].message.content)
 
 
